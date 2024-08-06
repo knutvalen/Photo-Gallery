eaaa91f945334ddbe72a1a11f2969318d9c30e3a
Knut Valen <knut.valen@icloud.com>
2024-08-06 10:39:00 +0200

    Take photos and display them in a gallery.

---
+++ src/hooks/usePhotoGallery.ts
@@ -0,0 +1,34 @@
+import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
+import { useState } from "react";
+
+export function usePhotoGallery() {
+  const [photos, setPhotos] = useState<UserPhoto[]>([]);
+
+  const takePhoto = async () => {
+    const photo = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+
+    const fileName = Date.now() + '.jpeg';
+    const newPhotos = [
+      {
+        filepath: fileName,
+        webviewPath: photo.webPath,
+      },
+      ...photos,
+    ];
+    setPhotos(newPhotos);
+  };
+
+  return {
+    photos,
+    takePhoto,
+  };
+}
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,8 +1,22 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import { camera } from 'ionicons/icons';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon, IonImg, IonCol, IonRow, IonGrid,
+} from "@ionic/react";
+import { usePhotoGallery } from "../hooks/usePhotoGallery";
+import React from "react";
 
 const Tab2: React.FC = () => {
+  const { photos, takePhoto } = usePhotoGallery();
+
   return (
     <IonPage>
       <IonHeader>
@@ -11,12 +25,20 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo) => (
+              <IonCol size="6" key={photo.filepath}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
       </IonContent>
     </IonPage>
   );
